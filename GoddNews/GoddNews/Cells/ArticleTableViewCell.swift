a432fdb9069a14899b74e6194f42292a2b97b800
HussamNagy <hussamnagyh@gmail.com>
2022-12-31 21:18:17 +0200

    Creating Custom Self Sizing Cells

---
+++ GoddNews/GoddNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleTableViewCell.swift
+//  GoddNews
+//
+//  Created by Hussam Nagy on 31/12/2022.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell:UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+}
